3121665fd042730135ccdbd3087c00fbc0531ad8
jeanbarcenas24 <152881024+jeanbarcenas24@users.noreply.github.com>
2023-12-15 15:18:28 +0800

    Add files via upload

---
+++ word.js
@@ -0,0 +1,34 @@
+const words = [
+    {
+        word : "Duck",
+        hint : "animal who lays eggs?"
+    },
+    {
+        word : "Ox",
+        hint : "A male cow"
+    },
+    {
+        word : "Water",
+        hint : "All animals need food, air, and ____ to survive."
+    },
+    {
+        word : "Cat",
+        hint : "fur-bearing animal"
+    },
+    {
+        word : "Moon",
+        hint : "Earth s only natural satellite"
+    },
+    {
+        word : "Leaves",
+        hint : "The tree has a branch filled with green _____."
+    },
+    {
+        word : "Muscles",
+        hint : "part of the body helps you move"
+    },
+    {
+        word : "Nostrils",
+        hint : "The two holes of the nose"
+    },
+]
